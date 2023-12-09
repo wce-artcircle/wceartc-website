53789b2a23418e008e8f336e3075d6edd67751c3
SuhaniGarud <suhanigarud234@gmail.com>
2023-12-09 14:42:42 +0530

    Responsive yuvarang page

--- Frontend/src/components/yuvarang/MainPage.jsx
+++ Frontend/src/components/yuvarang/MainPage.jsx
@@ -13,11 +13,10 @@ const MainPage = () => {
       <Navbar/>
       
       <div className="mainpage__container">
-
+          
         <div className="blurWrapper"></div> 
         <div className='bgImage'>
           <img src={title} alt=""/>
-          <button><a href='https://konfhub.com/yuvarang2024'>REGISTER</a></button>
         </div>
       </div>
       <div className='about-yuvarang'>
@@ -29,9 +28,10 @@ const MainPage = () => {
             <img src={poster} alt=""/>
             <div className='abt-yuva'>
               <p>
-              YUVARANG...here we create and you celebrate. It is a two day, annual mega event organised and conducted by Art Circle which is the only cultural club of WCE Sangli. This mega event consists of many interesting competitions like street plays, dance battles, poetry and many more. School level competitions like that of drawing, painting, creative writing are also organised. Each participant gets a certificate and the winners recieve amazing and heavy cash prizes. Every year this event attracts a mob of 2.5k. Here one gets a platform to exhibit his/her art, vibe up and compete with people having similar interests. People create communities and make connections strong. It is always said that Art is a way to embark. Revolving around this objective, the entire event turns into a place to simply innovate and exhilarate. It is a life time experience for participants as well as organisers. And finally YUVARANG is a festival where passion thrives, imagination gains life and emotion becomes memory to cherish alive.
+              YUVARANG...here we create and you celebrate. It is a two day, annual mega event organised and conducted by Art Circle which is the cultural club of WCE Sangli. This mega event consists of many interesting competitions like street plays, dance battles, poetry and many more. School level competitions like that of drawing, painting, creative writing are also organised. Each participant gets a certificate and the winners recieve amazing and heavy cash prizes. Every year this event attracts a mob of 2.5k. Here one gets a platform to exhibit his/her art, vibe up and compete with people having similar interests. People create communities and make connections strong. It is always said that Art is a way to embark. Revolving around this objective, the entire event turns into a place to simply innovate and exhilarate. It is a life time experience for participants as well as organisers. And finally YUVARANG is a festival where passion thrives, imagination gains life and emotion becomes memory to cherish alive.
               </p>
-              <a href='https://drive.google.com/file/d/1F1BrreCSLNTqbdDFAqdrn81C2HgC_LEP/view?usp=sharing'>View RuleBook</a>
+              <a href='https://drive.google.com/file/d/1Ik2wxTAtdaMnnA1T0SUxoYweDSSU2WGL/view?usp=drivesdk'>View RuleBook</a>
+              <a href='https://konfhub.com/yuvarang2024'>REGISTER</a>
             </div>
           </div>
         
