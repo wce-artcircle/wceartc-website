4553081c0a57f071c6ef844019fd7e5a514ad582
User-DK <daivikkarbhari01@gmail.com>
2025-01-24 11:18:06 +0530

    Add Sponsors

--- Frontend/src/Main.jsx
+++ Frontend/src/Main.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import Navbar from './components/nav/Navbar'
-import Landing from './components/landing/Landing'
-import About from './components/about/About'
-import Teams from './components/teams/Teams'
-import Events from './components/events/Events'
-import Members from './components/members/Members'
-import Footer from './components/footer/Footer'
-import Toplink from './components/yuvarang/Toplink'
+import React from "react";
+import Navbar from "./components/nav/Navbar";
+import Landing from "./components/landing/Landing";
+import About from "./components/about/About";
+import Teams from "./components/teams/Teams";
+import Events from "./components/events/Events";
+import Members from "./components/members/Members";
+import Footer from "./components/footer/Footer";
+import Toplink from "./components/yuvarang/Toplink";
+import SponsorsToplink from "./components/yuvarang/SponsorsToplink";
+import Sponsors from "./components/yuvarang/Sponsors";
 
 const Main = () => {
   return (
     <>
-    <Toplink/>
-    <Navbar/>
-    <Landing/>
-    <About/>
-    <Teams/>
-    <Events/>
-    <Members/>
-    <Footer/>
+      <Toplink />
+      <Navbar />
+      <SponsorsToplink />
+      <Landing />
+      <About />
+      <Teams />
+      <Sponsors />
+      <Events />
+      <Members />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
---
+++ Frontend/src/components/yuvarang/SponsorsToplink.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./Sponsors.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
+function SponsorsToplink() {
+  return (
+    <div className="marquee-container">
+      <b
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "10px",
+        }}
+      >
+        <u>Sponsors</u>
+      </b>
+
+      <a href="#sponsors">
+        <div className="marquee">
+          <a
+            href="https://www.dinnch.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="sponsors/refreshment.png" alt="Sponsor 5" />
+          </a>
+          <img src="sponsors/lead1.png" alt="Sponsor 1" />
+          <img src="sponsors/lead.png" alt="Sponsor 2" />
+          <img src="sponsors/co.png" alt="Sponsor 3" />
+          <img src="sponsors/food.png" alt="Sponsor 4" />
+
+          <img src="sponsors/media.png" alt="Sponsor 6" />
+          <img src="sponsors/facility.png" alt="Sponsor 7" />
+          <img src="sponsors/thank-you.png" alt="Sponsor 8" />
+          <div className="marquee-padding"></div>
+        </div>
+      </a>
+    </div>
+  );
+}
+
+export default SponsorsToplink;
+
+// // Add this CSS to your stylesheet or a <style> tag in your component
+// const styles = `
+// @keyframes borderAnimation {
+//   0% {
+//     border-color: transparent;
+//   }
+//   50% {
+//     border-color: blue;
+//   }
+//   100% {
+//     border-color: transparent;
+//   }
+// }
+// `;
+
+// // Inject styles into the document
+// const styleSheet = document.createElement("style");
+// styleSheet.type = "text/css";
+// styleSheet.innerText = styles;
+// document.head.appendChild(styleSheet);
