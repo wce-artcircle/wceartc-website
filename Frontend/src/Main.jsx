7bc10c158338b76d6b96e2990d0a4e41c3bff69d
AyushDMane <ayushdmane05@gmail.com>
2025-12-25 03:58:54 +0530

    Added popup for new event and minor changes in navbar

--- Frontend/src/Main.jsx
+++ Frontend/src/Main.jsx
@@ -9,6 +9,7 @@ import Footer from "./components/footer/Footer";
 import Toplink from "./components/yuvarang/Toplink";
 import SponsorsToplink from "./components/yuvarang/SponsorsToplink";
 import Sponsors from "./components/yuvarang/Sponsors";
+import ScrollToEvents from "./components/ScrollToEvents/ScrollToEvents.jsx";
 
 const Main = () => {
   return (
@@ -16,13 +17,41 @@ const Main = () => {
       <Toplink />
       <Navbar />
       <SponsorsToplink />
-      <Landing />
-      <About />
-      <Teams />
-      <Sponsors />
-      <Events />
-      <Members />
+      
+      {/* Landing Section */}
+      <section id="home">
+        <Landing />
+      </section>
+      
+      {/* About Section */}
+      <section id="about">
+        <About />
+      </section>
+      
+      {/* Teams Section */}
+      <section id="teams">
+        <Teams />
+      </section>
+      
+      {/* Sponsors Section */}
+      <section id="sponsors">
+        <Sponsors />
+      </section>
+      
+      {/* Events Section */}
+      <section id="events">
+        <Events />
+      </section>
+      
+      {/* Members Section */}
+      <section id="members">
+        <Members />
+      </section>
+      
       <Footer />
+      
+      {/* Floating Scroll to Events Button - appears on scroll */}
+      <ScrollToEvents />
     </>
   );
 };
